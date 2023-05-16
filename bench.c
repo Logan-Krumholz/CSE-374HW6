c4df26887a31df37c31a0c9534e3f9f4e6aa6b68
Logan-Krumholz <122186100+Logan-Krumholz@users.noreply.github.com>
2023-05-16 12:01:40 -0700

    Create bench.c

---
+++ bench.c
@@ -0,0 +1,58 @@
+/* bench.c is the benchmarking /test program for mem memory management
+   bench.c is implemented as part of HW6, CSE374 22WI
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+#include "mem.h"
+
+/* print_stats is a helper function that prints performance
+   information about the memory system.  Requires a clock_t start
+   time, and prints elapsed time plus results from get_mem_stats()
+*/
+void print_stats(clock_t start);
+
+/* fill_mem is a helper function that takes in an address and the
+   size of the memory allocation at that address.  It should fill
+   the first 16 bytes (or up to size) with hexedecimal values.*/
+void fill_mem(void* ptr, uintptr_t size);
+
+/* Synopsis:   bench (main)
+   [ntrials] (10000) getmem + freemem calls
+   [pctget] (50) % of calls that are get mem
+   [pctlarge] (10) % of calls requesting more memory than lower limit
+   [small_limit] (200) largest size in bytes of small block
+   [large_limit] (20000) largest size in byes of large block
+   [random_seed] (time) initial seed for randn
+*/
+int main(int argc, char** argv ) {
+  // Initialize the parameters
+  int NTRIALS;
+  int PCTGET;
+  int PCTLARGE;
+  int SMALL_L;
+  int LARGE_L;
+
+  /* write code to set the above values based on the input arguments
+     if there is not a corresponding input argument use the
+     default value in the spec. */
+
+
+  printf("Running bench for %d trials, %d%% getmem calls.\n", NTRIALS, PCTGET);
+
+  void* blocks[NTRIALS];  // upperbound block storage
+  int ntrials = 0, nblocks = 0;
+  clock_t start = clock();
+
+  // perform NTRIALS mem operations
+
+  /* Students will fill in this section according to the assignment
+     specs.*/
+
+  return EXIT_SUCCESS;
+}
+
+/* Define helper functions here. */
+
